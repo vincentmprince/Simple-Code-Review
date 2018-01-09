2750c7c9066a68da33efe69eed5a8056fdc63bd7
vincentmprince <35266725+vincentmprince@users.noreply.github.com>
2018-01-09 09:50:26 -0700

    created walmart greeter

--- CSharp Code Review/CodeReview/Program.cs
+++ CSharp Code Review/CodeReview/Program.cs
@@ -10,6 +10,11 @@ namespace CodeReview
     {
         static void Main(string[] args)
         {
+            // Use the Salutation class for a Walmart Greeter
+            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
+            // Get the guy to talk
+            Console.WriteLine(oldGuy.Greet());
+            Console.WriteLine(oldGuy.SayFarewell());
         }
     }
 }
--- CSharp Code Review/CodeReview/Salutation.cs
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace CodeReview
 {
-    class Salutation
+    public class Salutation
     {
+
+        private string _Greeting;
+        private string _Farewell;
+        public Salutation (string greetingMessage, string farewellMessage)
+        {
+            _Greeting = greetingMessage;
+            _Farewell = farewellMessage;
+        }
+        public string Greet()
+        {
+            return _Greeting;
+        }
+        public string SayFarewell()
+        {
+            return _Farewell;
+        }
     }
 }
