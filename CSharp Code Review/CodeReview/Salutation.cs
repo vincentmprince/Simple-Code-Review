0e488ab6ddb0b5bba7961f22d3952a33c4d2b885
vincentmprince <35266725+vincentmprince@users.noreply.github.com>
2018-01-11 16:02:50 -0700

    simple app

--- CSharp Code Review/CodeReview/Salutation.cs
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace CodeReview
 {
+    /// <summary>
+    /// XML comment - The Saluatation class represents something that can say hello or goodbye
+    /// </summary>
     public class Salutation
     {
+        //  A computer program is a set of instructions for manipulating information
+        //  A class is a blueprint for creating objects
 
+        #region Properties and Fields - What the salutation objects will "look like"
+        //  Fields
         private string _Greeting;
         private string _Farewell;
+        #endregion
+        #region Constructor and Methods - How the salutation objects will "behave"
+        /// <summary>
+        /// Instantiates a Salutation object with appropriate messages
+        /// </summary>
+        /// <param name="greetingMessage">The message to use when greeting</param>
+        /// <param name="farewellMessage">The message to use to say goodbye</param>
         public Salutation (string greetingMessage, string farewellMessage)
         {
             _Greeting = greetingMessage;
@@ -24,5 +38,7 @@ namespace CodeReview
         {
             return _Farewell;
         }
+        #endregion
     }
+
 }
